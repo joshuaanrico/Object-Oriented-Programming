8cfd0ad69d5fb069b05f5c75e1373278f30b8c3c
Laurensius Joshua Anrico Agustinus <joshua.anrico30@gmail.com>
2019-11-20 22:19:20 +0700

    Polymorph

---
+++ Polymorph/Lamborgini.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Polymorph;
+
+/**
+ *
+ * @author LAURENSIUS
+ */
+public class Lamborgini extends Merk{
+    public void Merk(){
+        System.out.println("Lamborgini");
+    }
+}
---
+++ Polymorph/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Polymorph;
+
+/**
+ *
+ * @author LAURENSIUS
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Merk m = new Merk();
+        Merk lambo = new Lamborgini();
+        Merk ferari = new Ferrari();
+        
+        m.Merk();
+        lambo.Merk();
+        ferari.Merk();
+    }
+}
